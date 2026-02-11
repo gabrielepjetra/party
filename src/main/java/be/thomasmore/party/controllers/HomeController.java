96451bf922cc311fb2deea344bf7298843841b0e
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-02-11 19:15:47 +0100

    excercise 3.6 - pay page added with new request handler

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
 
     @GetMapping({"/","/home"})
     public String home(Model model) {
         int myCalculatedValue = 34 * 62;
+
         model.addAttribute("myCalculatedValue", myCalculatedValue);
+
         return "home";
     }
 
@@ -20,10 +25,25 @@ public class HomeController {
         String myAddress = "VIIde-Olympiadelaan 17";
         String myCity = "Antwerp";
         String myCountry = "Belgium";
+
         model.addAttribute("myName", myName);
         model.addAttribute("myAddress", myAddress);
         model.addAttribute("myCity", myCity);
         model.addAttribute("myCountry", myCountry);
+
         return "about";
+    }
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        LocalDate today = LocalDate.now();
+        LocalDate futureDay = today.plusDays(30);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        model.addAttribute("today", today.format(formatter));
+        model.addAttribute("futureDay", futureDay.format(formatter));
+
+        return "pay";
     }
 }
