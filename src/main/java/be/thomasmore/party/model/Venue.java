67a9c8a6684dd0e87d265b8ea8429952f75244e8
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-02-15 21:17:44 +0100

    excercise 5.1 - 5.5 - created database and inserted test values

--- src/main/java/be/thomasmore/party/model/Venue.java
+++ src/main/java/be/thomasmore/party/model/Venue.java
@@ -1,6 +1,17 @@
 package be.thomasmore.party.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Venue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
     private String venueName;
     private String linkMoreInfo;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO VENUE (VENUE_NAME, LINK_MORE_INFO)
+values ('test-venue','test-link');
