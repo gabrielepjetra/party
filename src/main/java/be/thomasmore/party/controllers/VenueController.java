06debe70d03a1733035d9ee16ef9822762cec05b
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-02-15 21:29:31 +0100

    excercise 6.2 - venue repository created

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -1,21 +1,27 @@
 package be.thomasmore.party.controllers;
 
 import be.thomasmore.party.model.Venue;
+import be.thomasmore.party.repositories.VenueRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
 public class VenueController {
+    private final VenueRepository venueRepository;
+
+    public VenueController(VenueRepository venueRepository) {
+        this.venueRepository = venueRepository;
+    }
 
     @GetMapping("/venuedetails")
     public String venueDetails(Model model) {
-        Venue venue = new Venue();
-
-        venue.setVenueName("BoesjKammeree");
-        venue.setLinkMoreInfo("https://www.barboesj.be/");
-
-        model.addAttribute("venue", venue);
+        Optional<Venue> venueFromDb = venueRepository.findById(1);
+        if (venueFromDb.isPresent()) {
+            model.addAttribute("venue", venueFromDb.get());
+        }
 
         return "venuedetails";
     }
