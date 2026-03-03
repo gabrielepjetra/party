f6a4f0b848eb7c6c31e5bc0905905e0dbec52c95
gabriele pjetra <gabriele.pjetra@student.thomasmore.be>
2026-03-03 16:09:43 +0100

    bundel 3 - 6.1 query uitgebreid om artiesten te zoeken om basis van verschillende parameter

--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -7,6 +7,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ArtistRepository extends CrudRepository<Artist,Integer> {
-    @Query("select a from Artist a where :keyword is null or lower(a.artistName) like lower(concat('%', :keyword, '%'))")
+    @Query("select a from Artist a where :keyword is null or " +
+            "lower(a.artistName) like lower(concat('%', :keyword, '%')) or " +
+            "lower(a.bio) like lower(concat('%', :keyword, '%')) or " +
+            "lower(a.genre) like lower(concat('%', :keyword, '%')) or " +
+            "lower(a.portfolio) like lower(concat('%', :keyword, '%'))")
     List<Artist> findByFilter(@Param("keyword") String keyword);
 }
